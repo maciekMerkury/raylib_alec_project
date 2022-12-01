5307b98b4acb626d082bff6f1faddb3f359ff4f8
alecazem <alec@pop-os.localdomain>
2022-12-01 19:53:37 +0100

    systems things

---
+++ src/systems/systems.c
@@ -0,0 +1,10 @@
+#include "systems.h"
+
+#include "raymath.h"
+
+void move_pos(Position *const pos, Velocity *const vel, int entity_count, float d_t)
+{
+  for (int i = 0; i < entity_count; i++) {
+    pos[i] = Vector2Add(pos[i], Vector2Scale(vel[i], d_t));
+  }
+}
---
+++ src/systems/systems.h
@@ -0,0 +1,4 @@
+#include "components.h"
+
+void move_pos(Position *pos, Velocity *vel, int entity_count, float d_t);
+
