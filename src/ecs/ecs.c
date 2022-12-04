f922b74071d30943c936e11d56831d1f3242671f
maciekMerkury <maciekmerkury2@gmail.com>
2022-12-04 15:11:16 +0100

    add operations on the state and handy macros for using them

--- src/ecs/ecs.c
+++ src/ecs/ecs.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -10,16 +11,31 @@
 
 #define REALLOC_INCREASE 8
 
+/// set handles to null and free memory
+void
+reset_components(EcsState *const state, EntityId id)
+{
+    if (id.id > state->entity_count) {
+        printf("trying to access an invalid entity. id = %lu, entity_count = %lu\n", id.id, state->entity_count);
+        exit(1);
+    }
+
+    for (int i = 0; i < state->component_type_count; ++i) {
+        free(state->components[i].data[id.id]);
+        state->components[i].data[id.id] = NULL;
+    }
+}
+
 EcsState
 ecs_init(size_t init_cap, ...)
 {
     EcsState state = {0};
+    state.entity_capacity = init_cap;
 
     va_list ap;
     va_start(ap, init_cap);
 
-    int i = 0;
-    for (size_t size = va_arg(ap, size_t); size != 0; size = va_arg(ap, size_t)) {
+    for (int size = va_arg(ap, size_t); size > -1; size = va_arg(ap, size_t)) {
         void** data = calloc(sizeof(void*), init_cap);
         if (!data) {
             printf("[%s; %d]:\tcould not malloc %lu bytes\n", __FILE__, __LINE__, sizeof(void*) * init_cap);
@@ -27,16 +43,13 @@ ecs_init(size_t init_cap, ...)
             exit(1);
         }
 
-        state.components[i] = (CompactContainer){.element_size = size, .cap = init_cap, .data = data};
+        state.components[state.component_type_count] = (CompactContainer){.element_size = size, .data = data};
 
-        if (++i >= MAXIMUM_COMPONENT_COUNT) {
+        if (++state.component_type_count >= MAXIMUM_COMPONENT_COUNT) {
             printf("No more than 64 component types are permitted, see ecs.h for more details\n");
             exit(1);
         }
     }
-
-    state.component_type_count = i;
-
     va_end(ap);
 
     return state;
@@ -47,42 +60,33 @@ init_entity(EcsState* const state, ...)
 {
     EntityId id;
 
-    if (is_empty(&state->empty_entity_queue)) {
-        id = pop(&state->empty_entity_queue);
+    if (!is_empty(&state->dead_entity_queue)) {
+        id = pop(&state->dead_entity_queue);
     } else {
         id = (EntityId){state->entity_count++};
 
-        // increase the cap and realloc the array if needed
-        for (int i = 0; i < state->component_type_count; ++i) {
+        if (state->entity_count > state->entity_capacity) {
+            state->entity_capacity += REALLOC_INCREASE;
 
-            // increase in size is needed
-            if (id.id > state->components[i].cap) {
-                CompactContainer* cont = &state->components[i];
+            for (int i = 0; i < state->component_type_count; ++i) {
+                CompactContainer *cont = &state->components[i];
 
-                cont->cap += REALLOC_INCREASE;
-
-                void* new = reallocarray(cont->data, cont->cap, sizeof(void*));
-
-                if (!new) {
-                    printf("could not resize the component type %i. new size was supposed to be %lu\n", i, cont->cap);
+                cont->data = reallocarray(cont->data, state->entity_capacity, sizeof(void*));
+                if (!cont->data) {
+                    printf("could not resize the component type %i. new size was supposed to be %lu\n", i, state->entity_capacity);
+                    perror("malloc error: ");
                     exit(1);
                 }
-
-                cont->data = new;
             }
         }
     }
 
-    /// set handles to null and free memory
-    for (int i = 0; i < state->component_type_count; ++i) {
-        free(get_element(&state->components[i], id.id));
-        state->components[i].data[id.id] = NULL;
-    }
+    reset_components(state, id);
 
     va_list ap;
     va_start(ap, state);
 
-    for (int i = va_arg(ap, size_t); i != 0; i = va_arg(ap, size_t)) {
+    for (int i = va_arg(ap, size_t); i > -1; i = va_arg(ap, size_t)) {
         if (i >= state->component_type_count) {
             printf("trying to access component out of bounds. i = %i, component_type_count = %i\n", i,
                    state->component_type_count);
@@ -96,3 +100,94 @@ init_entity(EcsState* const state, ...)
 
     return id;
 }
+
+void
+kill_entity(EcsState *const state, EntityId entity)
+{
+    if (entity.id >= state->entity_count) {
+        printf("trying to kill an entity outside of entity_count. id = %lu, count = %lu", entity.id, state->entity_count);
+        exit(1);
+    }
+
+    reset_components(state, entity);
+    push(&state->dead_entity_queue, entity);
+}
+
+QueryResult
+get_all_components(EcsState* const state, ...)
+{
+
+    size_t component_type_count = 0;
+    int *component_indices = calloc(state->component_type_count, sizeof(int));
+    CompactContainer **components = calloc(state->component_type_count, sizeof(CompactContainer*));
+
+    va_list ap;
+    va_start(ap, state);
+
+    // TODO: finish this
+    for (int i = va_arg(ap, int); i > -1; i = va_arg(ap, int)) {
+        if (i >= state->component_type_count) {
+            printf("trying to query component out of bounds. i = %i, type_count = %i\n", i, state->component_type_count);
+            exit(1);
+        }
+
+        components[component_type_count] = &state->components[i];
+        component_indices[component_type_count] = i;
+
+        component_type_count++;
+    }
+    va_end(ap);
+
+    // Shrink the arrays to not waste memory
+    component_indices = reallocarray(component_indices, component_type_count, sizeof(int));
+    components = reallocarray(components, component_type_count, sizeof(CompactContainer));
+
+    return (QueryResult){
+        .component_type_count = component_type_count,
+        .component_indices = component_indices,
+        .components = components
+    };
+}
+
+EntityQueryResult
+get_entity_components(EcsState *const state, EntityId entity, ...)
+{
+    if (entity.id > state->entity_count) {
+        printf("trying to query for an entity out of entity_count. id = %lu, entity_count = %lu\n", entity.id, state->entity_count);
+        exit(1);
+    }
+
+    size_t component_type_count = 0;
+    int *component_indices = calloc(state->component_type_count, sizeof(int));
+    GenericComponent *components = calloc(state->component_type_count, sizeof(GenericComponent));
+
+    va_list ap;
+    va_start(ap, entity);
+
+    for (int i = va_arg(ap, int); i > -1; i = va_arg(ap, int)) {
+        if (i >= state->component_type_count) {
+            printf("trying to query component out of bounds. i = %i, type_count = %i\n", i, state->component_type_count);
+            exit(1);
+        }
+
+        components[component_type_count] = (GenericComponent) {
+            .inner_size = state->components[i].element_size,
+            .data = state->components[i].data[entity.id],
+        };
+        component_indices[component_type_count] = i;
+
+        component_type_count++;
+    }
+
+    va_end(ap);
+
+    component_indices = reallocarray(component_indices, component_type_count, sizeof(int));
+    components = reallocarray(components, component_type_count, sizeof(GenericComponent));
+
+    return (EntityQueryResult) {
+        .component_type_count = component_type_count,
+        .component_indices = component_indices,
+        .components = components
+    };
+}
+
--- src/ecs/ecs.h
+++ src/ecs/ecs.h
@@ -39,13 +39,28 @@ typedef struct {
 
 
 #define todo(thing) static_assert(false, "unfinished: " #thing "\n")
-#define SHORT_ECS_INIT(init_cap, ...) ecs_init(init_cap, __VA_ARGS__, NULL)
 
 /// initialises the ecs using with the component order given.
 /// the last va_arg must be a 0, otherwise function will not work correctly
 __attribute__((sentinel)) EcsState ecs_init(size_t init_cap, ...);
+#define SHORT_ECS_INIT(init_cap, ...) ecs_init(init_cap, __VA_ARGS__, NULL)
 
 /// creates an entity with components provided.
-/// returns the EntityId to the newly-created entity
+/// returns the EntityId to the newly-created entity.
+/// SAFETY: the value before sentinel must be -1. sentinel will not be used, but is included as a reminder to use -1
 __attribute__((sentinel)) EntityId init_entity(EcsState* const state, ...);
+#define SHORT_INIT_ENTITY(state, ...) init_entity(state, __VA_ARGS__, -1, NULL)
+
+void kill_entity(EcsState *const state, EntityId entity);
+
+/// SAFETY: the value before sentinel must be -1. sentinel will not be used, but is included as a reminder to use -1
+__attribute__((sentinel)) QueryResult get_all_components(EcsState* const state, ...);
+#define ECS_GET_ALL_COMPONENTS(state, ...) get_all_components(state, __VA_ARGS__, -1, NULL)
+
+// TODO: think about whether indexing should be done via mask or va args
+//EntityQueryResult get_entity_components(EcsState *const state, uint64_t mask);
+
+/// SAFETY: the value before sentinel must be -1. sentinel will not be used, but is included as a reminder to use -1
+__attribute__((sentinel)) EntityQueryResult get_entity_components(EcsState *const state, EntityId entity, ...);
+#define ECS_GET_ENTITY_COMPONENTS(state, entity, ...) get_entity_components(state, entity, __VA_ARGS__, -1, NULL)
 
