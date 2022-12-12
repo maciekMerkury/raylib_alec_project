75f9d0751186d7de6ea3fb51098f4479d3f49081
maciekMerkury <maciekmerkury2@gmail.com>
2022-12-12 17:39:25 +0100

    Added tests

---
+++ src/tests/ecs_tests.c
@@ -0,0 +1,15 @@
+#include "../ecs/ecs.h"
+#include <stdio.h>
+
+typedef struct {
+    float x, y;
+} Vector2;
+
+
+int
+main()
+{
+    EcsState state = SHORT_ECS_INIT(init_cap, );
+    return 0;
+}
+
